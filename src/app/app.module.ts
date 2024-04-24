215efd978d3fb32f96ff733fbceedb45b6d90e5f
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-04-24 16:39:22 -0300

    Curso Angular #05: Módulos (ngModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';//prepara a aplicacão para ser executada
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './primeiro-projeto/meu-primeiro.component';
 
 
-@NgModule({
-  declarations: [
+@NgModule({ //decoreidor
+  declarations: [ //declaração de todos os componentes,diretidas e pipes
     AppComponent,
     MeuPrimeiroComponent,
   ],
-  imports: [
+  imports: [ // adiciona os modulos que serao utilizados na aplicação
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
+  providers: [ // serviços que ficaram disponiveis para todos componentes
     provideClientHydration()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // 
 })
-export class AppModule { }
+export class AppModule { } //declaracão da classe

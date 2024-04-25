ea473a2d495a57af255908859627e1d91d176f1f
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-04-25 10:52:19 -0300

    Curso Angular #06: Introdução aos Templates

--- primeiro-projeto/src/cursos/cursos.component.ts
+++ primeiro-projeto/src/cursos/cursos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
 
+    nomePortal: string;
+    cursos: string[] = ['Java', 'Ext JS', 'angular'];
+
+    constructor(){
+      this.nomePortal = 'https://www.youtube.com';
+
+      for(let i=0; i<this.cursos.length; i++){
+        let curso = this.cursos[i];
+      }
+    }
+
+    ngOnInit(){
+
+    }
 }

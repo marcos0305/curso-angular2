8dc671ffb4b18c056b4b1c6cc4a98c2cfea1477c
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-05-08 15:10:21 -0300

    Curso Angular #11: Event binding

--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -12,14 +12,33 @@ export class DataBindingComponent {
   urlImagem: any = 'https://images.app.goo.gl/C2ohYHVsGk9VMWLS6';
 
   cursoAngular: boolean = true;
+
+  valorAtual: string ='';
+  valorSalvo ='';
   
+  isMouseOver: boolean = false;
+
   getGosteiCurso(){
     return true;
   }
+
+  botaoClicado(){
+    alert('Botão clicado');
+  }
+
   getValor(){
     return  1;
   }
 
-  
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+  }
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 
 }

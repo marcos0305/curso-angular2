8286817f94e2715c984d41d08e4beceb7772729d
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-10-15 20:02:09 -0300

    Descrição do commit

---
+++ 06-pipes/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    BrowserModule,  // Outros módulos que sua aplicação usa
+  ],
+  providers: [],
+  bootstrap: []  // Componente de bootstrap principal
+})
+export class AppModule { }
